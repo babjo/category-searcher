7513680a178d7510ff8592970dc02bab107f0144
babjo <kd980311@naver.com>
2014-01-01 16:43:59 +0900

    Change-Id: I421a230a13c2c25514f02b905ab7c3a40f3091e5

---
+++ src/main/java/lch/category_searcher/App.java
@@ -0,0 +1,13 @@
+package lch.category_searcher;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
